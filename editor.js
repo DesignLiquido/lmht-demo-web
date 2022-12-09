fbc3a2ac318a2128e2c52c36868819a00983d09b
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2022-12-09 12:54:44 -0800

    Não usando mais lmht-js porque a dependência saxon-js não funciona bem no navegador de internet.

--- editor.js
+++ editor.js
@@ -1,16 +1,30 @@
-const lmhtWeb = new window.Lmht.LmhtWeb();
-
-window.monaco.editor.create(document.getElementById("containerLmht"), {
-    value: "<lmht>\n\t<cabeça></cabeça>\n\t<corpo></corpo>\n\t</lmht>",
+const editorLmht = window.monaco.editor.create(document.getElementById("containerLmht"), {
+    value: "<lmht>\n\t<cabeça>\n\t\t<titulo>Olá mundo</titulo>\n\t</cabeça>\n\t<corpo>\n\t\t<p>Olá mundo</p>\n\t</corpo>\n</lmht>",
     language: "lmht"
 });
+const modeloEditorLmht = editorLmht.getModel();
 
-lmhtWeb.converter("<lmht>\n\t<cabeça></cabeça>\n\t<corpo></corpo>\n\t</lmht>").then(resposta => {
-    console.log(resposta)
+window.serializadorXml = new XMLSerializer();
+
+const transformacaoInicial = SaxonJS.transform({ 
+    stylesheetLocation: "lmht.sef.json",
+    sourceText: modeloEditorLmht.getValue()
 });
 
-
-window.monaco.editor.create(document.getElementById("containerHtml"), {
-    value: '',
+const editorHtml = window.monaco.editor.create(document.getElementById("containerHtml"), {
+    value: window.serializadorXml.serializeToString(transformacaoInicial.principalResult),
     language: "html"
 });
+const modeloEditorHtml = editorHtml.getModel();
+
+modeloEditorLmht.onDidChangeContent(() => {
+    const transformacao = SaxonJS.transform({ 
+        stylesheetLocation: "lmht.sef.json",
+        sourceText: modeloEditorLmht.getValue()
+    });
+
+    modeloEditorHtml.setValue(
+        window.serializadorXml.serializeToString(transformacao.principalResult)
+    );
+});
+
