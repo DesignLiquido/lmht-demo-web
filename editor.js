49b4a464778e46c99a1c4748b85ca5faec38136c
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-12-27 16:53:43 -0800

    Updates

--- editor.js
+++ editor.js
@@ -16,15 +16,40 @@ const editorHtml = window.monaco.editor.create(document.getElementById("containe
     language: "html"
 });
 const modeloEditorHtml = editorHtml.getModel();
+window.atualizando = 0
 
 modeloEditorLmht.onDidChangeContent(() => {
+    if (window.atualizando > 0) {
+        window.atualizando = 0;
+        return;
+    }
+
     const transformacao = SaxonJS.transform({ 
         stylesheetLocation: "lmht.sef.json",
         sourceText: modeloEditorLmht.getValue()
     });
+
+    window.atualizando = 1;
 
     modeloEditorHtml.setValue(
         window.serializadorXml.serializeToString(transformacao.principalResult)
     );
 });
 
+modeloEditorHtml.onDidChangeContent(() => {
+    if (window.atualizando > 0) {
+        window.atualizando = 0;
+        return;
+    }
+
+    const transformacao = SaxonJS.transform({ 
+        stylesheetLocation: "lmht-reverso.sef.json",
+        sourceText: modeloEditorHtml.getValue()
+    });
+
+    window.atualizando = 1;
+
+    modeloEditorLmht.setValue(
+        window.serializadorXml.serializeToString(transformacao.principalResult)
+    );
+});
